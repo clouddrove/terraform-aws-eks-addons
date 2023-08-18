0bda2ab2972915bf6eaa15392a6bd969aeec5268
dipak2515 <dipakyadav725625@gmail.com>
2023-08-18 14:11:58 +0530

    feat: ingress-nginx, version 4.6.1

--- _examples/complete/variables.tf
+++ _examples/complete/variables.tf
@@ -73,8 +73,5 @@ variable "externalsecrets_manifests" {
 #--------------INGRESS NGINX------------------------
 variable "nginx_ingress_extra_configs" {
   type = any
-  default = {
-    namespace        = "test"
-    create_namespace = false
-  }
+  default = {}
 }
