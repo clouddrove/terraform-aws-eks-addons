3f4a5e34f1f292fd91f90fb16c8d0798d0d5b4b5
nileshgadgi <gadginileshp@gmail.com>
2023-09-26 22:41:22 +0530

    fix: remove dependency error

--- addons/kube-state-metrics/main.tf
+++ addons/kube-state-metrics/main.tf
@@ -4,6 +4,4 @@ module "helm_addon" {
   manage_via_gitops = var.manage_via_gitops
   helm_config       = local.helm_config
   addon_context     = var.addon_context
-
-  depends_on = [kubernetes_namespace_v1.this]
 }
