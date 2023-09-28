3145b83a100031936a4f6924dffefa00739828af
ballalamit <ballalamit2@gmail.com>
2023-09-28 20:39:44 +0530

    fix- changed the version in readme file

--- addons/keda/locals.tf
+++ addons/keda/locals.tf
@@ -13,7 +13,7 @@ locals {
     lint                       = try(var.keda_extra_configs.lint, "false")
     repository_key_file        = try(var.keda_extra_configs.repository_key_file, "")
     repository_cert_file       = try(var.keda_extra_configs.repository_cert_file, "")
-    repository_username        = try(var.keda_extra_configs.repository_password, "")
+    repository_username        = try(var.keda_extra_configs.repository_username, "")
     repository_password        = try(var.keda_extra_configs.repository_password, "")
     verify                     = try(var.keda_extra_configs.verify, "false")
     keyring                    = try(var.keda_extra_configs.keyring, "")
