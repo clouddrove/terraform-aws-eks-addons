1f91192d24c4ded37bfe05b430ee0ef50c90335e
VishwajitNagulkar <nagulkarvishwajit1999@gmail.com>
2023-10-04 15:44:46 +0530

    Feat: 🚀 removed unnecessary attributes from cert-manager

--- addons/cert-manager/main.tf
+++ addons/cert-manager/main.tf
@@ -4,8 +4,6 @@ module "helm_addon" {
   manage_via_gitops = var.manage_via_gitops
   helm_config       = local.helm_config
   addon_context     = var.addon_context
-
-  depends_on = [kubernetes_namespace_v1.this]
 }
 
 resource "kubernetes_namespace_v1" "this" {
