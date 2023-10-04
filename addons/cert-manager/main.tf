9e70ed76c1a8bf77ed7a8c3591bb1112e2b7adbf
VishwajitNagulkar <nagulkarvishwajit1999@gmail.com>
2023-10-04 14:53:32 +0530

    Feat: 🚀 Added metadata option

--- addons/cert-manager/main.tf
+++ addons/cert-manager/main.tf
@@ -11,4 +11,7 @@ module "helm_addon" {
 resource "kubernetes_namespace_v1" "this" {
   count = try(local.helm_config["create_namespace"], true) && local.helm_config["namespace"] != "cert-manager" ? 1 : 0
 
+  metadata {
+    name = local.helm_config["namespace"]
+  }
 }
