7a0628b5db45aeda9dd0c85ff5f12d49769f1532
ramnivas-cd <ram.nivas@clouddrove.com>
2026-03-20 20:50:45 +0530

    chore: updated terraform addon (karpenter) & workflow readme for terraform docs

--- examples/complete/locals.tf
+++ examples/complete/locals.tf
@@ -2,7 +2,7 @@ locals {
   region          = "us-east-1"
   name            = "helm-addons"
   environment     = "test"
-  cluster_version = "1.32"
+  cluster_version = "1.34"
   vpc_cidr        = "10.0.0.0/16"
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   tags = {
