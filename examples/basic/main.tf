db73a3a227f1b26e7a21c110293ca6f396fe90ec
Anmol Nagpal <anmol@clouddrove.com>
2026-05-29 23:06:50 +0530

    fix: terraform format

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -181,14 +181,14 @@ module "http_https" {
     to_port     = 0
     cidr_ipv6   = "::/0"
     description = "Allow all traffic."
-  },
-  {
-    key         = 2
-    from_port   = 0
-    ip_protocol = "-1"
-    to_port     = 0
-    cidr_ipv4   = "0.0.0.0/0"
-    description = "Allow all traffic."
+    },
+    {
+      key         = 2
+      from_port   = 0
+      ip_protocol = "-1"
+      to_port     = 0
+      cidr_ipv4   = "0.0.0.0/0"
+      description = "Allow all traffic."
   }]
 }
 
